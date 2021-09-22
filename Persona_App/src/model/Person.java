7d222b037062d67394afd11f449829a20da1aa21
Piyush Kumar Sultania <sultania.p@northeastern.edu>
2021-09-22 17:15:10 -0400

    JPanel Created and Person class defined

--- Persona_App/src/model/Person.java
+++ Persona_App/src/model/Person.java
@@ -11,4 +11,216 @@ package model;
  */
 public class Person {
     
+    private String FirstName;
+    private String MiddleName;
+    private String LastName;
+    private String MailingAddressLine1;
+    private String MailingAddressLine2;
+    private String City;
+    private String State;
+    private Integer ZipCode;
+    private String Country;
+    private String DateOfBirth;
+    private Long TelephoneNumber;
+    private Long CellPhoneNumber;
+    private Integer FaxNumber;
+    private String EmailAddress;
+    private Long SocialSecurityNumber;
+    private String MedicalRecordNumber;
+    private String HealthPlanBeneficiaryNumber;
+    private Long BankAccountNumber;
+    private String CertificateOrLicenseNumber;
+    private String VehicleIdentifiersAndSerialNumbers;
+    private String DeviceIdentifiersAndSerialNumbers;
+    private String LinkedInAddress;
+    private String IPAddress;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String FirstName) {
+        this.FirstName = FirstName;
+    }
+
+    public String getMiddleName() {
+        return MiddleName;
+    }
+
+    public void setMiddleName(String MiddleName) {
+        this.MiddleName = MiddleName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
+    }
+
+    public String getMailingAddressLine1() {
+        return MailingAddressLine1;
+    }
+
+    public void setMailingAddressLine1(String MailingAddressLine1) {
+        this.MailingAddressLine1 = MailingAddressLine1;
+    }
+
+    public String getMailingAddressLine2() {
+        return MailingAddressLine2;
+    }
+
+    public void setMailingAddressLine2(String MailingAddressLine2) {
+        this.MailingAddressLine2 = MailingAddressLine2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public Integer getZipCode() {
+        return ZipCode;
+    }
+
+    public void setZipCode(Integer ZipCode) {
+        this.ZipCode = ZipCode;
+    }
+
+    public String getCountry() {
+        return Country;
+    }
+
+    public void setCountry(String Country) {
+        this.Country = Country;
+    }
+
+    public String getDateOfBirth() {
+        return DateOfBirth;
+    }
+
+    public void setDateOfBirth(String DateOfBirth) {
+        this.DateOfBirth = DateOfBirth;
+    }
+
+    public Long getTelephoneNumber() {
+        return TelephoneNumber;
+    }
+
+    public void setTelephoneNumber(Long TelephoneNumber) {
+        this.TelephoneNumber = TelephoneNumber;
+    }
+
+    public Long getCellPhoneNumber() {
+        return CellPhoneNumber;
+    }
+
+    public void setCellPhoneNumber(Long CellPhoneNumber) {
+        this.CellPhoneNumber = CellPhoneNumber;
+    }
+
+    public Integer getFaxNumber() {
+        return FaxNumber;
+    }
+
+    public void setFaxNumber(Integer FaxNumber) {
+        this.FaxNumber = FaxNumber;
+    }
+
+    public String getEmailAddress() {
+        return EmailAddress;
+    }
+
+    public void setEmailAddress(String EmailAddress) {
+        this.EmailAddress = EmailAddress;
+    }
+
+    public Long getSocialSecurityNumber() {
+        return SocialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(Long SocialSecurityNumber) {
+        this.SocialSecurityNumber = SocialSecurityNumber;
+    }
+
+    public String getMedicalRecordNumber() {
+        return MedicalRecordNumber;
+    }
+
+    public void setMedicalRecordNumber(String MedicalRecordNumber) {
+        this.MedicalRecordNumber = MedicalRecordNumber;
+    }
+
+    public String getHealthPlanBeneficiaryNumber() {
+        return HealthPlanBeneficiaryNumber;
+    }
+
+    public void setHealthPlanBeneficiaryNumber(String HealthPlanBeneficiaryNumber) {
+        this.HealthPlanBeneficiaryNumber = HealthPlanBeneficiaryNumber;
+    }
+
+    public Long getBankAccountNumber() {
+        return BankAccountNumber;
+    }
+
+    public void setBankAccountNumber(Long BankAccountNumber) {
+        this.BankAccountNumber = BankAccountNumber;
+    }
+
+    public String getCertificateOrLicenseNumber() {
+        return CertificateOrLicenseNumber;
+    }
+
+    public void setCertificateOrLicenseNumber(String CertificateOrLicenseNumber) {
+        this.CertificateOrLicenseNumber = CertificateOrLicenseNumber;
+    }
+
+    public String getVehicleIdentifiersAndSerialNumbers() {
+        return VehicleIdentifiersAndSerialNumbers;
+    }
+
+    public void setVehicleIdentifiersAndSerialNumbers(String VehicleIdentifiersAndSerialNumbers) {
+        this.VehicleIdentifiersAndSerialNumbers = VehicleIdentifiersAndSerialNumbers;
+    }
+
+    public String getDeviceIdentifiersAndSerialNumbers() {
+        return DeviceIdentifiersAndSerialNumbers;
+    }
+
+    public void setDeviceIdentifiersAndSerialNumbers(String DeviceIdentifiersAndSerialNumbers) {
+        this.DeviceIdentifiersAndSerialNumbers = DeviceIdentifiersAndSerialNumbers;
+    }
+
+    public String getLinkedInAddress() {
+        return LinkedInAddress;
+    }
+
+    public void setLinkedInAddress(String LinkedInAddress) {
+        this.LinkedInAddress = LinkedInAddress;
+    }
+
+    public String getIPAddress() {
+        return IPAddress;
+    }
+
+    public void setIPAddress(String IPAddress) {
+        this.IPAddress = IPAddress;
+    }
+    
+    
+    
+    
+    
 }
--- Persona_App/src/ui/MainJFrame.java
+++ Persona_App/src/ui/MainJFrame.java
@@ -9,7 +9,7 @@ package ui;
  *
  * @author piyus
  */
-public class MainJFrame extends javax.swing.JPanel {
+public class MainJFrame extends javax.swing.JFrame {
 
     /**
      * Creates new form MainJFrame
@@ -27,19 +27,111 @@ public class MainJFrame extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-        this.setLayout(layout);
+        splitPane = new javax.swing.JSplitPane();
+        controlPanel = new javax.swing.JPanel();
+        btnCreate = new javax.swing.JButton();
+        btnView = new javax.swing.JButton();
+        workArea = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        btnCreate.setText("Create");
+
+        btnView.setText("View");
+
+        javax.swing.GroupLayout controlPanelLayout = new javax.swing.GroupLayout(controlPanel);
+        controlPanel.setLayout(controlPanelLayout);
+        controlPanelLayout.setHorizontalGroup(
+            controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(controlPanelLayout.createSequentialGroup()
+                .addContainerGap(17, Short.MAX_VALUE)
+                .addGroup(controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnView, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnCreate, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 73, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+        );
+
+        controlPanelLayout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {btnCreate, btnView});
+
+        controlPanelLayout.setVerticalGroup(
+            controlPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(controlPanelLayout.createSequentialGroup()
+                .addGap(73, 73, 73)
+                .addComponent(btnCreate)
+                .addGap(26, 26, 26)
+                .addComponent(btnView)
+                .addContainerGap(153, Short.MAX_VALUE))
+        );
+
+        splitPane.setLeftComponent(controlPanel);
+
+        javax.swing.GroupLayout workAreaLayout = new javax.swing.GroupLayout(workArea);
+        workArea.setLayout(workAreaLayout);
+        workAreaLayout.setHorizontalGroup(
+            workAreaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 293, Short.MAX_VALUE)
+        );
+        workAreaLayout.setVerticalGroup(
+            workAreaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 298, Short.MAX_VALUE)
+        );
+
+        splitPane.setRightComponent(workArea);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(splitPane)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(splitPane)
         );
+
+        pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainJFrame().setVisible(true);
+            }
+        });
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnCreate;
+    private javax.swing.JButton btnView;
+    private javax.swing.JPanel controlPanel;
+    private javax.swing.JSplitPane splitPane;
+    private javax.swing.JPanel workArea;
     // End of variables declaration//GEN-END:variables
 }
